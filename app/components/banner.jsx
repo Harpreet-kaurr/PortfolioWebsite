58c8948b11d0cbfaf94b8fdd1c1a2ccf80357170
Harpreet-kaurr <kaurhap1995@gmail.com>
2025-10-20 14:22:39 -0400

    added

--- app/components/banner.jsx
+++ app/components/banner.jsx
@@ -3,14 +3,13 @@ import DownArrow from '../svgs/DownArrow'
 import CanvasCursor from './CanvasCursor'
 const banner = () => {
   return (
-      <section className="bg-black flex flex-col gap-6 items-center justify-center min-h-screen w-full px-4" id="hero">
+      <section  id="hero" className="bg-black flex flex-col gap-6 items-center justify-center min-h-screen w-full px-4">
       <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl text-white text-center">
         Hello, I&apos;m <span className="text-red-500">Harpreet.</span>
         <br />
         I&apos;m a full stack web developer.
       </h1>
       <CanvasCursor />
-
       <a href='#projects'
         className="flex items-center justify-center text-red-500 border-2 border-red-500 py-2 px-6 font-semibold 
           w-full max-w-[200px] transform transition-all duration-300 hover:-translate-y-1 cursor-pointer"
