a20e2d9f0f1a0eb57fc5cbf6c19568765e033ec9
Harpreet-kaurr <kaurhap1995@gmail.com>
2025-10-22 12:39:28 -0400

    canvas cursor added

--- app/components/banner.jsx
+++ app/components/banner.jsx
@@ -9,7 +9,7 @@ const banner = () => {
         <br />
         I&apos;m a full stack web developer.
       </h1>
-      {/* <CanvasCursor /> */}
+      <CanvasCursor />
       <a href='#projects'
         className="flex items-center justify-center text-red-500 border-2 border-red-500 py-2 px-6 font-semibold 
           w-full max-w-[200px] transform transition-all duration-300 hover:-translate-y-1 cursor-pointer"
